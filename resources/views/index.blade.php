d8c25099f2c317dc12149eba596f8461c6d56ea7
pengab <dazzabrah1738@gmail.com>
2023-06-15 21:52:41 +0700

    perbaikan tampilan home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,21 +18,21 @@
         </h3>
         <div class="list-ruangan d-flex flex-wrap justify-content-center">
             <div class="card m-3" style="width: 18rem;">
-                <img src="img/ruang-kelas.jpeg" style="height: 250px" class="card-img-top" alt="Paracetamol">
+                <img src="img/Paracetamol.png" style="height: 250px" class="card-img-top" alt="Paracetamol">
                 <div class="card-body">
                   <h5 class="card-title text-center">Paracetamol</h5>
                   <p class="card-text">Paracetamol adalah obat untuk meredakan demam dan nyeri ringan hingga sedang, misalnya sakit kepala, nyeri haid, atau pegal-pegal.</p>
                 </div>
               </div>
               <div class="card m-3" style="width: 18rem;">
-                <img src="img/lab-komputer.jpeg" style="height: 250px" class="card-img-top" alt="Amoxicillin">
+                <img src="img/Amoxicillin.png" style="height: 250px" class="card-img-top" alt="Amoxicillin">
                 <div class="card-body">
                   <h5 class="card-title text-center">Amoxicillin</h5>
                   <p class="card-text">Amoxicillin adalah obat antibiotik yang digunakan untuk mengatasi berbagai penyakit akibat infeksi bakteri.</p>
                 </div>
               </div>
               <div class="card m-3" style="width: 18rem;">
-                <img src="img/noPhoto.png" style="height: 250px" class="card-img-top" alt="Cetirizine">
+                <img src="img/Cetirizine.png" style="height: 250px" class="card-img-top" alt="Cetirizine">
                 <div class="card-body">
                   <h5 class="card-title text-center">Cetirizine</h5>
                   <p class="card-text">Cetirizine adalah obat untuk meredakan gejala akibat reaksi alergi, seperti mata berair, bersin-bersin, hidung meler, atau gatal di kulit.</p>
