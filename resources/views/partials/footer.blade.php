203eaf8a24caa65e35e536bc580ee40e518a7e5a
irsydwibwo <galaxytron35@gmail.com>
2023-06-15 15:18:02 +0700

    partials views

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,8 @@
+ <!-- Start Footer -->
+ <footer class="footer d-flex justify-content-center">
+      <div class="container">
+        &copy; Copyright <strong><span>Apotek</span></strong
+        >. All Rights Reserved
+      </div>
+    </footer>
+    <!-- End Footer -->
