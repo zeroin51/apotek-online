215fdf972a90aac7253ad11cc3287ace86c375dc
zeroin51 <davstwn88@gmail.com>
2023-06-15 15:53:19 +0700

    perbaikan pada model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,29 +17,35 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+     protected $guarded = ['id'];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+     /**
+      * The attributes that should be hidden for serialization.
+      *
+      * @var array<int, string>
+      */
+     protected $hidden = [
+         'password',
+         'remember_token',
+     ];
+ 
+     /**
+      * The attributes that should be cast.
+      *
+      * @var array<string, string>
+      */
+     protected $casts = [
+         'email_verified_at' => 'datetime',
+     ];
+ 
+     public function stock()
+     {
+         return $this->hasMany(Stock::class);
+     }
+ 
+     public function role()
+     {
+         return $this->belongsTo(Role::class);
+     }
 }
