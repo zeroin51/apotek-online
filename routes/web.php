bd888c8f4b3607fcb8404e8a25d259e7d8afb2a2
satrioaryow <satrio.wcksn2005@gmail.com>
2023-06-15 15:31:58 +0700

    routes web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardStockController;
+use App\Http\Controllers\DashboardUserController;
+use App\Http\Controllers\DashboardAdminController;
+use App\Http\Controllers\DashboardObatController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\StockMasukController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +34,16 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('dashboard/stocks/{id}/endAddStock', [DashboardStockController::class, 'endAddStock'])->middleware('auth');
 
 Route::resource('dashboard/stocks', DashboardStockController::class)->middleware('auth');
+
+Route::resource('dashboard/obats', DashboardObatController::class)->middleware('auth');
+
+Route::get('dashboard/users/{id}/makeAdmin', [DashboardUserController::class, 'makeAdmin'])->middleware('auth');
+
+Route::resource('dashboard/users', DashboardUserController::class)->middleware('auth');
+
+Route::get('dashboard/admin/{id}/removeAdmin', [DashboardAdminController::class, 'removeAdmin'])->middleware('auth');
+
+Route::resource('dashboard/admin', DashboardAdminController::class)->middleware('auth');
 
 Route::get('/dashboard/stockmasuk', [StockMasukController::class, 'index'])->middleware('auth');
 
